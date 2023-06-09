7bb6d8aef6f93ac7dc4933241fdca5081a72181e
Karl Seguin <k@openmymind.io>
2023-06-09 16:19:39 +0800

    Expose configs
    Config.Cors is now public
    
    Request.Config and Response.Config are now also available as Config.Request
    and Config.Response. This allows an application to access all config types,
    Config, Config.Cors, Config.Request, Config.Response, via httpz.Config

--- src/config.zig
+++ src/config.zig
@@ -2,14 +2,19 @@ const request = @import("request.zig");
 const response = @import("response.zig");
 
 pub const Config = struct {
+	// done like this so that an external app can access it as
+	// httpz.Config.Request or httpz.Config.Response
+	pub const Request = request.Config;
+	pub const Response = response.Config;
+
 	port: ?u16 = null,
 	pool_size: ?u16 = null,
 	address: ?[]const u8 = null,
-	request: request.Config = request.Config{},
-	response: response.Config = response.Config{},
+	request: Request = Request{},
+	response: Response = Response{},
 	cors: ?CORS = null,
 
-	const CORS = struct {
+	pub const CORS = struct {
 		origin: []const u8,
 		headers: ?[]const u8 = null,
 		methods: ?[]const u8 = null,
