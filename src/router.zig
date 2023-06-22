4e1240eda17cf67e7e1ef2dea458e3e26463cdc7
Karl Seguin <k@openmymind.io>
2023-06-22 10:15:10 +0800

    update zig (builtins changed names)

--- src/router.zig
+++ src/router.zig
@@ -554,7 +554,7 @@ test "route: root" {
 
 	// test "all" route
 	inline for (@typeInfo(httpz.Method).Enum.fields) |field| {
-		const m = @intToEnum(httpz.Method, field.value);
+		const m = @enumFromInt(httpz.Method, field.value);
 		try t.expectEqual(&testRoute4, router.route(m, urls[2], &params).?.action);
 	}
 }
