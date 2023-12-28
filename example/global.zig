9b8ae0f7036b409ebea2424cb9f175f9ed0dbdbd
يزن حمزه <yznhamzeh@gmail.com>
2023-12-28 16:59:13 +0300

    zig fmt

--- example/global.zig
+++ example/global.zig
@@ -4,27 +4,27 @@ const Allocator = std.mem.Allocator;
 
 // Our global state
 const GlobalContext = struct {
-	hits: usize = 0,
-	l: std.Thread.Mutex = .{},
+    hits: usize = 0,
+    l: std.Thread.Mutex = .{},
 
-	pub fn increment(self: *GlobalContext, _: *httpz.Request, res: *httpz.Response) !void {
-		self.l.lock();
-		const hits = self.hits + 1;
-		self.hits = hits;
-		self.l.unlock();
+    pub fn increment(self: *GlobalContext, _: *httpz.Request, res: *httpz.Response) !void {
+        self.l.lock();
+        const hits = self.hits + 1;
+        self.hits = hits;
+        self.l.unlock();
 
-		res.content_type = httpz.ContentType.TEXT;
-		res.body = try std.fmt.allocPrint(res.arena, "{d} hits", .{hits});
-	}
+        res.content_type = httpz.ContentType.TEXT;
+        res.body = try std.fmt.allocPrint(res.arena, "{d} hits", .{hits});
+    }
 };
 
 // Started in main.zig which starts 3 servers, on 3 different ports, to showcase
 // small variations in using httpz.
 pub fn start(allocator: Allocator) !void {
-	var ctx = GlobalContext{};
-	var server = try httpz.ServerCtx(*GlobalContext, *GlobalContext).init(allocator, .{.port = 5883}, &ctx);
-	defer server.deinit();
-	var router = server.router();
-	router.get("/increment", GlobalContext.increment);
-	return server.listen();
+    var ctx = GlobalContext{};
+    var server = try httpz.ServerCtx(*GlobalContext, *GlobalContext).init(allocator, .{ .port = 5883 }, &ctx);
+    defer server.deinit();
+    var router = server.router();
+    router.get("/increment", GlobalContext.increment);
+    return server.listen();
 }
--- src/config.zig
+++ src/config.zig
@@ -2,58 +2,58 @@ const request = @import("request.zig");
 const response = @import("response.zig");
 
 pub const Config = struct {
-	port: ?u16 = null,
-	address: ?[]const u8 = null,
-	unix_path: ?[]const u8 = null,
-	workers: Worker = .{},
-	request: Request = .{},
-	response: Response = .{},
-	timeout: Timeout = .{},
-	cors: ?CORS = null,
-	websocket: ?Websocket = null,
+    port: ?u16 = null,
+    address: ?[]const u8 = null,
+    unix_path: ?[]const u8 = null,
+    workers: Worker = .{},
+    request: Request = .{},
+    response: Response = .{},
+    timeout: Timeout = .{},
+    cors: ?CORS = null,
+    websocket: ?Websocket = null,
 
-	pub const Worker = struct {
-		count: ?u16 = null,
-		max_conn: ?u16 = null,
-		min_conn: ?u16 = null,
-		large_buffer_count: ?u16 = null,
-		large_buffer_size: ?u32 = null,
-	};
+    pub const Worker = struct {
+        count: ?u16 = null,
+        max_conn: ?u16 = null,
+        min_conn: ?u16 = null,
+        large_buffer_count: ?u16 = null,
+        large_buffer_size: ?u32 = null,
+    };
 
-	pub const Request = struct {
-		max_body_size: ?usize = null,
-		buffer_size: ?usize = null,
-		max_header_count: ?usize = null,
-		max_param_count: ?usize = null,
-		max_query_count: ?usize = null,
-	};
+    pub const Request = struct {
+        max_body_size: ?usize = null,
+        buffer_size: ?usize = null,
+        max_header_count: ?usize = null,
+        max_param_count: ?usize = null,
+        max_query_count: ?usize = null,
+    };
 
-	pub const Response = struct {
-		max_header_count: ?usize = null,
-		body_buffer_size: ?usize = null,
-		header_buffer_size: ?usize = null,
-	};
+    pub const Response = struct {
+        max_header_count: ?usize = null,
+        body_buffer_size: ?usize = null,
+        header_buffer_size: ?usize = null,
+    };
 
-	pub const Timeout = struct {
-		request: ?u32 = null,
-		keepalive: ?u32 = null,
-		request_count: ?u32 = null,
-	};
+    pub const Timeout = struct {
+        request: ?u32 = null,
+        keepalive: ?u32 = null,
+        request_count: ?u32 = null,
+    };
 
-	pub const CORS = struct {
-		origin: []const u8,
-		headers: ?[]const u8 = null,
-		methods: ?[]const u8 = null,
-		max_age: ?[]const u8 = null,
-	};
+    pub const CORS = struct {
+        origin: []const u8,
+        headers: ?[]const u8 = null,
+        methods: ?[]const u8 = null,
+        max_age: ?[]const u8 = null,
+    };
 
-	pub const Websocket = struct {
-		max_size: usize = 65536,
-		buffer_size: usize = 4096,
-		handle_ping: bool = false,
-		handle_pong: bool = false,
-		handle_close: bool = false,
-		large_buffer_pool_count: u16 = 8,
-		large_buffer_size: usize = 32768,
-	};
+    pub const Websocket = struct {
+        max_size: usize = 65536,
+        buffer_size: usize = 4096,
+        handle_ping: bool = false,
+        handle_pong: bool = false,
+        handle_close: bool = false,
+        large_buffer_pool_count: u16 = 8,
+        large_buffer_size: usize = 32768,
+    };
 };
