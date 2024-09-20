97b0acda34e9fbc02c6cf6f5abd091c7e0daaee9
Karl Seguin <k@openmymind.io>
2024-09-20 10:57:51 +0800

    fix threadpool batching

--- src/thread_pool.zig
+++ src/thread_pool.zig
@@ -59,7 +59,7 @@ pub fn ThreadPool(comptime F: anytype) type {
 
         // we expect allocator to be an Arena
         pub fn init(allocator: Allocator, opts: Opts) !*Self {
-            const queue = try allocator.alloc(Args, opts.backlog);
+            const queue = try allocator.alloc(Args, if (opts.backlog == 0 or opts.backlog == 1) 2 else opts.backlog);
             const threads = try allocator.alloc(Thread, opts.count);
             const thread_pool = try allocator.create(Self);
 
@@ -140,17 +140,16 @@ pub fn ThreadPool(comptime F: anytype) type {
                 const ready = if (capacity >= pending.len) pending else pending[0..capacity];
                 for (ready) |a| {
                     queue[head] = a;
-                    // std.debug.print("{d}\n", .{head});
                     head = if (head == queue_end) 0 else head + 1;
                 }
                 self.head = head;
                 self.mutex.unlock();
+                self.read_cond.signal();
                 if (ready.len == pending.len) {
                     break;
                 }
                 pending = pending[ready.len..];
             }
-            self.read_cond.signal();
         }
 
         // Having a re-usable buffer per thread is the most efficient way
@@ -192,11 +191,54 @@ pub fn ThreadPool(comptime F: anytype) type {
 }
 
 const t = @import("t.zig");
+test "ThreadPool: batch add" {
+    defer t.reset();
+
+    const counts = [_]u32{1, 2, 3, 4, 5, 6};
+    const backlogs = [_]u32{1, 2, 3, 4, 5, 6};
+    for (counts) |count| {
+        for (backlogs) |backlog| {
+            testSum = 0; // global defined near the end of this file
+            testCount = 0; // global defined near the end of this file
+            testC1 = 0;
+            testC2 = 0;
+            testC3 = 0;
+            testC4 = 0;
+            testC5 = 0;
+            testC6 = 0;
+            var tp = try ThreadPool(testIncr).init(t.arena.allocator(), .{ .count = count, .backlog = backlog, .buffer_size = 512 });
+
+            for (0..1_000) |_| {
+                tp.spawn(&.{.{1}, .{2}, .{3}, .{4}});
+            }
+            while (tp.empty() == false) {
+                std.time.sleep(std.time.ns_per_ms);
+            }
+            tp.stop();
+            try t.expectEqual(10_000, testSum);
+            try t.expectEqual(4_000, testCount);
+
+            try t.expectEqual(1000, testC1);
+            try t.expectEqual(1000, testC2);
+            try t.expectEqual(1000, testC3);
+            try t.expectEqual(1000, testC4);
+            try t.expectEqual(0, testC5);
+            try t.expectEqual(0, testC6);
+        }
+    }
+}
+
 test "ThreadPool: small fuzz" {
     defer t.reset();
 
     testSum = 0; // global defined near the end of this file
     testCount = 0; // global defined near the end of this file
+    testC1 = 0;
+    testC2 = 0;
+    testC3 = 0;
+    testC4 = 0;
+    testC5 = 0;
+    testC6 = 0;
     var tp = try ThreadPool(testIncr).init(t.arena.allocator(), .{ .count = 3, .backlog = 3, .buffer_size = 512 });
 
     for (0..10_000) |_| {
@@ -208,6 +250,12 @@ test "ThreadPool: small fuzz" {
     tp.stop();
     try t.expectEqual(60_000, testSum);
     try t.expectEqual(30_000, testCount);
+    try t.expectEqual(10_000, testC1);
+    try t.expectEqual(10_000, testC2);
+    try t.expectEqual(10_000, testC3);
+    try t.expectEqual(0, testC4);
+    try t.expectEqual(0, testC5);
+    try t.expectEqual(0, testC6);
 }
 
 test "ThreadPool: large fuzz" {
@@ -215,6 +263,12 @@ test "ThreadPool: large fuzz" {
 
     testSum = 0; // global defined near the end of this file
     testCount = 0; // global defined near the end of this file
+    testC1 = 0;
+    testC2 = 0;
+    testC3 = 0;
+    testC4 = 0;
+    testC5 = 0;
+    testC6 = 0;
     var tp = try ThreadPool(testIncr).init(t.arena.allocator(), .{ .count = 50, .backlog = 1000, .buffer_size = 512 });
 
     for (0..10_000) |_| {
@@ -228,14 +282,36 @@ test "ThreadPool: large fuzz" {
     tp.stop();
     try t.expectEqual(210_000, testSum);
     try t.expectEqual(60_000, testCount);
+    try t.expectEqual(10_000, testC1);
+    try t.expectEqual(10_000, testC2);
+    try t.expectEqual(10_000, testC3);
+    try t.expectEqual(10_000, testC4);
+    try t.expectEqual(10_000, testC5);
+    try t.expectEqual(10_000, testC6);
+
 }
 
 var testSum: u64 = 0;
 var testCount: u64 = 0;
+var testC1: u64 = 0;
+var testC2: u64 = 0;
+var testC3: u64 = 0;
+var testC4: u64 = 0;
+var testC5: u64 = 0;
+var testC6: u64 = 0;
 fn testIncr(c: u64, buf: []u8) void {
     std.debug.assert(buf.len == 512);
     _ = @atomicRmw(u64, &testSum, .Add, c, .monotonic);
     _ = @atomicRmw(u64, &testCount, .Add, 1, .monotonic);
+    switch (c) {
+        1 => _ = @atomicRmw(u64, &testC1, .Add, 1, .monotonic),
+        2 => _ = @atomicRmw(u64, &testC2, .Add, 1, .monotonic),
+        3 => _ = @atomicRmw(u64, &testC3, .Add, 1, .monotonic),
+        4 => _ = @atomicRmw(u64, &testC4, .Add, 1, .monotonic),
+        5 => _ = @atomicRmw(u64, &testC5, .Add, 1, .monotonic),
+        6 => _ = @atomicRmw(u64, &testC6, .Add, 1, .monotonic),
+        else => unreachable,
+    }
     // let the threadpool queue get backed up
-    std.time.sleep(std.time.ns_per_us * 100);
+    std.time.sleep(std.time.ns_per_us * 20);
 }
