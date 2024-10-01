d852fe37f68a754ddd0d0791473feb2d822eede6
Karl Seguin <k@openmymind.io>
2024-10-01 09:49:38 +0800

    adjust for Zig's removal of std.builtin.default_panic

--- test_runner.zig
+++ test_runner.zig
@@ -299,7 +299,7 @@ pub fn panic(msg: []const u8, error_return_trace: ?*std.builtin.StackTrace, ret_
     if (current_test) |ct| {
         std.debug.print("\x1b[31m{s}\npanic running \"{s}\"\n{s}\x1b[0m\n", .{BORDER, ct, BORDER});
     }
-    std.builtin.default_panic(msg, error_return_trace, ret_addr);
+    std.builtin.Panic.call(msg, error_return_trace, ret_addr);
 }
 
 fn isUnnamed(t: std.builtin.TestFn) bool {
