0f5696e6cc81306b1c76a889d789125f65a6e25e
stacksmash <stacksmash@unixwizard.dev>
2025-02-11 00:57:02 -0500

    update for new Allocator API change

--- src/httpz.zig
+++ src/httpz.zig
@@ -691,32 +691,40 @@ const FallbackAllocator = struct {
                 .alloc = alloc,
                 .resize = resize,
                 .free = free,
+                .remap = remap,
             },
         };
     }
 
-    fn alloc(ctx: *anyopaque, len: usize, ptr_align: u8, ra: usize) ?[*]u8 {
+    fn alloc(ctx: *anyopaque, len: usize, alignment: std.mem.Alignment, ra: usize) ?[*]u8 {
         const self: *FallbackAllocator = @ptrCast(@alignCast(ctx));
-        return self.fixed.rawAlloc(len, ptr_align, ra) orelse self.fallback.rawAlloc(len, ptr_align, ra);
+        return self.fixed.rawAlloc(len, alignment, ra) orelse self.fallback.rawAlloc(len, alignment, ra);
     }
 
-    fn resize(ctx: *anyopaque, buf: []u8, buf_align: u8, new_len: usize, ra: usize) bool {
+    fn resize(ctx: *anyopaque, buf: []u8, alignment: std.mem.Alignment, new_len: usize, ra: usize) bool {
         const self: *FallbackAllocator = @ptrCast(@alignCast(ctx));
         if (self.fba.ownsPtr(buf.ptr)) {
-            if (self.fixed.rawResize(buf, buf_align, new_len, ra)) {
+            if (self.fixed.rawResize(buf, alignment, new_len, ra)) {
                 return true;
             }
         }
-        return self.fallback.rawResize(buf, buf_align, new_len, ra);
+        return self.fallback.rawResize(buf, alignment, new_len, ra);
     }
 
-    fn free(ctx: *anyopaque, buf: []u8, buf_align: u8, ra: usize) void {
+    fn free(ctx: *anyopaque, buf: []u8, alignment: std.mem.Alignment, ra: usize) void {
         _ = ctx;
         _ = buf;
-        _ = buf_align;
+        _ = alignment;
         _ = ra;
         // hack.
         // Always noop since, in our specific usage, we know fallback is an arena.
+    }
+
+    fn remap(ctx: *anyopaque, memory: []u8, alignment: std.mem.Alignment, new_len: usize, ret_addr: usize) ?[*]u8 {
+        if (resize(ctx, memory, alignment, new_len, ret_addr)) {
+            return memory.ptr;
+        }
+        return null;
     }
 };
 
