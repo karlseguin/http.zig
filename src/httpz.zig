04e381fc6d56530e2cfad451bdf798dd5d32c6b1
Karl Seguin <k@openmymind.io>
2025-05-31 13:36:53 +0800

    add res.setStatus(std.http.Status) helper
    https://github.com/karlseguin/http.zig/issues/128

--- src/httpz.zig
+++ src/httpz.zig
@@ -1608,7 +1608,7 @@ const TestDummyHandler = struct {
     }
 
     fn jsonRes(_: *Request, res: *Response) !void {
-        res.status = 201;
+        res.setStatus(.created);
         try res.json(.{ .over = 9000, .teg = "soup" }, .{});
     }
 
