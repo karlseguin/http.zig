06e4fd06c9c3584335bff07d1f3d90da886f3521
ItsMeSamey <92444995+ItsMeSamey@users.noreply.github.com>
2025-06-14 16:15:44 +0530

    key_value.zig and params.zig: fix memory leaks and (#138)
    * key_value.zig and params.zig: fix memory leaks and
        use single allocation
    
    params.zig and key_value.zig: could leak memory if
    2nd (or 3rd) allocation failed. This is now fixed.
    Number of Allocations is also reduced to 1.
    
    * made the changes to key_value and params non-breaking
    
    changes to key_value and params no longer break
    anything on the user side.

--- src/key_value.zig
+++ src/key_value.zig
@@ -4,36 +4,42 @@ const mem = std.mem;
 const ascii = std.ascii;
 const Allocator = std.mem.Allocator;
 
-fn KeyValue(K: type, V: type, equalFn: fn (lhs: K, rhs: K) callconv(.Inline) bool, hashFn: fn (key: K) callconv(.Inline) u8) type {
+fn KeyValue(V: type, hashFn: fn (key: []const u8) callconv(.Inline) u8) type {
     return struct {
         len: usize,
-        keys: []K,
+        keys: [][]const u8,
         values: []V,
         hashes: []u8,
 
+        const Self = @This();
         pub const Value = V;
 
-        const Self = @This();
+        const alignment = @max(@alignOf([]const u8), @alignOf(V));
+        const size = @sizeOf([]const u8) + @sizeOf(V) + @sizeOf(u8);
+        const kFirst = @alignOf([]const u8) >= @alignOf(V);
 
-        pub fn init(allocator: Allocator, max: usize) !Self {
+        pub fn init(allocator: Allocator, max: usize) Allocator.Error!Self {
+            // we want type with bigger alignment to be first.
+            // Since alignment is always a power of 2, the second type is guaranteed to have correct alignment.
+            const allocation = try allocator.alignedAlloc(u8, alignment, max * size);
             return .{
                 .len = 0,
-                .keys = try allocator.alloc(K, max),
-                .values = try allocator.alloc(V, max),
-                .hashes = try allocator.alloc(u8, max),
+                .keys = @as([*][]const u8, @alignCast(@ptrCast(if (kFirst) allocation.ptr else allocation[max * @sizeOf(V)..].ptr)))[0..max],
+                .values = @as([*]V, @alignCast(@ptrCast(if (kFirst) allocation[max * @sizeOf([]const u8)..].ptr else allocation.ptr)))[0..max],
+                .hashes = allocation[max * @sizeOf([]const u8) + max * @sizeOf(V)..],
             };
         }
 
         pub fn deinit(self: *Self, allocator: Allocator) void {
-            allocator.free(self.keys);
-            allocator.free(self.values);
-            allocator.free(self.hashes);
+            const allocation = @as([*] align(alignment) u8, @alignCast(@ptrCast(if (kFirst) self.keys.ptr else self.values.ptr)));
+            allocator.free(allocation[0..self.keys.len * size]);
         }
 
-        pub fn add(self: *Self, key: K, value: V) void {
+        pub fn add(self: *Self, key: []const u8, value: V) void {
             const len = self.len;
+            const max = self.keys.len;
             var keys = self.keys;
-            if (len == keys.len) {
+            if (len == max) {
                 return;
             }
 
@@ -43,24 +49,18 @@ fn KeyValue(K: type, V: type, equalFn: fn (lhs: K, rhs: K) callconv(.Inline) boo
             self.len = len + 1;
         }
 
-        pub fn get(self: *const Self, key: K) ?V {
+        pub fn get(self: *const Self, key: []const u8) ?V {
             const hash = hashFn(key);
             for (self.hashes[0..self.len], 0..) |h, i| {
-                if (h == hash and equalFn(self.keys[i], key)) {
+                if (h == hash and std.mem.eql(u8, self.keys[i], key)) {
                     return self.values[i];
                 }
             }
             return null;
         }
 
-        pub fn has(self: *const Self, key: K) bool {
-            const hash = hashFn(key);
-            for (self.hashes[0..self.len], 0..) |h, i| {
-                if (h == hash and equalFn(self.keys[i], key)) {
-                    return true;
-                }
-            }
-            return false;
+        pub fn has(self: *const Self, key: []const u8) bool {
+            return self.get(key) != null;
         }
 
         pub fn reset(self: *Self) void {
@@ -109,17 +109,13 @@ inline fn strHash(key: []const u8) u8 {
     return @as(u8, @truncate(key.len)) | (key[0]) ^ (key[key.len - 1]);
 }
 
-inline fn strEql(lhs: []const u8, rhs: []const u8) bool {
-    return std.mem.eql(u8, lhs, rhs);
-}
-
-pub const StringKeyValue = KeyValue([]const u8, []const u8, strEql, strHash);
+pub const StringKeyValue = KeyValue([]const u8, strHash);
 
 const MultiForm = struct {
     value: []const u8,
     filename: ?[]const u8 = null,
 };
-pub const MultiFormKeyValue = KeyValue([]const u8, MultiForm, strEql, strHash);
+pub const MultiFormKeyValue = KeyValue(MultiForm, strHash);
 
 const t = @import("t.zig");
 test "KeyValue: get" {
