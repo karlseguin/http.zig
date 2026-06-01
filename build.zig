3f80c729675b2c81c45356fad9b304bca1d4831b
Kamil Tomsik <info@tomsik.cz>
2026-06-01 13:29:08 +0200

    zig17: fix build.zig & SafeAllocator.init()

--- build.zig
+++ build.zig
@@ -91,9 +91,7 @@ pub fn build(b: *std.Build) !void {
 
             const run_cmd = b.addRunArtifact(exe);
             run_cmd.step.dependOn(b.getInstallStep());
-            if (b.args) |args| {
-                run_cmd.addArgs(args);
-            }
+            run_cmd.addPassthruArgs();
 
             const run_step = b.step(ex.name, ex.file);
             run_step.dependOn(&run_cmd.step);
